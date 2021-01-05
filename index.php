216bfea44d43309e6742e32cb92825ff99e8671f
dayfutue <76985442+dayfutue@users.noreply.github.com>
2021-01-05 12:10:13 +0800

    upload index.php
    update index

---
+++ index.php
@@ -0,0 +1,256 @@
+<!doctype html>
+<html lang="en" id="home">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <!-- css external -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- css fancybox -->
+    <link rel="stylesheet" type="text/css" href="plugins/fancybox/jquery.fancybox.css">
+
+    <title>Dewata Travel</title>
+  </head>
+  <body class="mt-5">
+  	<!-- navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    	<div class="container">
+  			<a class="navbar-brand page-scroll" href="#home">Dewata Travel</a>
+  			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    		<span class="navbar-toggler-icon"></span>
+  			</button>
+  			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    			<div class="navbar-nav justify-content-end">
+      				<a class="nav-item nav-link active page-scroll" href="#about">About <span class="sr-only">(current)</span></a>
+     				<a class="nav-item nav-link page-scroll" href="#portfolio">Activity</a>
+      				<a class="nav-item nav-link page-scroll" href="#contact">Contact</a>
+   				 </div>
+ 			</div>
+ 		</div>
+	</nav>
+	<!-- end navbar -->
+
+	<!-- jumbotron -->
+	<div class="jumbotron jumbotron-fluid">
+  		<div class="container text-center">
+  			<img src="img/logo-dewata.png" width="25%" class="rounded-circle img-thumbnail">
+   			<h1 class="display-4">Dewata Travel</h1>
+   			<p class="lead">tour harian, transportasi, grup rekreasi, tour tambahan, dan perjalanan pendidikan</p>
+  		</div>
+	</div>
+	<!-- end jumbotron -->
+
+	<!-- about -->
+	<section id="about" class="about">
+	<div class="container">
+		<div class="row mb-4 pt-4">
+			<div class="col-sm text-center">
+				<h1 class="display-4">About Us</h1>
+				<hr>
+			</div>
+		</div>
+
+		<div class="row justify-content-center">
+			<div class="col-sm-5 pKiri">
+				<h4>About Dewata Travel.</h4>
+				<p class="text-justify">Saat ini kami berkembang ke seluruh Bali, kami mendekati setiap tur, perjalanan, atau pengalaman dengan perspektif yang dikembangkan dari rumah sendiri. 95% dari orang-orang kami adalah penduduk setempat - memadukan pengetahuan tentang tempat-tempat yang dikunjungi mitra kami dan wisatawan mereka dengan pemahaman intuitif tentang pengalaman perjalanan yang mereka harapkan. Karena kami tahu apa yang membuat pengalaman itu begitu istimewa.</p>
+			</div>
+			<div class="col-sm-5 pKanan">
+				<a data-fancybox="gallery" href="img/peta.png">
+			  		<img class="card-img-top img img-responsive" src="img/peta.png" alt="Card image cap">
+			  	</a>
+			</div>
+		</div>
+	</div>
+	</section>
+	<!-- end about -->
+
+	<!-- portfolio -->
+	<section id="portfolio" class="portfolio bg-light pb-4">
+	<div class="container">
+		<div class="row mb-4 pt-4">
+			<div class="col text-center">
+				<h1 class="display-4">Explore</h1>
+				<hr>
+			</div>
+		</div>
+		<div class="row mb-4">
+			<div class="col-sm">
+				<div class="card">
+					<div class="popout">
+						<a data-fancybox="gallery" href="img/thumbs/wisata4.png">
+			  				<img class="card-img-top img img-responsive" src="img/thumbs/wisata4.png" alt="Card image cap">
+			  			</a>
+			  			<div class="card-body">
+			   				<h5 class="card-title">Kabupaten Badung</h5>
+			   				<p class="card-text">Kabupaten Badung memiliki begitu banyak tempat wisata yang menarik.</p>
+			    			<a href="destination.php?daerah_des=Badung" name="badung" class="btn btn-info">Go Explore</a>
+			 			 </div>
+			 		</div>
+				</div>
+			</div>
+			<div class="col-sm">
+				<div class="card">
+					<div class="popout">
+			  			<a data-fancybox="gallery" href="img/thumbs/wisata2.png">
+			  				<img class="card-img-top img img-responsive" src="img/thumbs/wisata2.png" alt="Card image cap">
+			  			</a>
+			  			<div class="card-body">
+			   				<h5 class="card-title">Kabupaten Tabanan</h5>
+			   				<p class="card-text">Kabupaten Tabanan memiliki begitu banyak tempat wisata yang menarik.</p>
+			    			<a href="destination.php?daerah_des=Tabanan" name="tabanan" class="btn btn-info">Go Explore</a>
+			 			 </div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm">
+				<div class="card">
+					<div class="popout">
+			  			<a data-fancybox="gallery" href="img/thumbs/wisata7.png">
+			  				<img class="card-img-top img img-responsive" src="img/thumbs/wisata7.png" alt="Card image cap">
+			  			</a>
+			  			<div class="card-body">
+			   				<h5 class="card-title">Kabupaten Singaraja</h5>
+			   				<p class="card-text">Kabupaten Singaraja memiliki begitu banyak tempat wisata yang menarik.</p>
+			    			<a href="destination.php" class="btn btn-info">Go Explore</a>
+			 			</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	
+		<div class="row mb-4">
+			<div class="col-sm">
+				<div class="card">
+					<div class="popout">
+			  			<a data-fancybox="gallery" href="img/thumbs/wisata3.png">
+			  				<img class="card-img-top img img-responsive" src="img/thumbs/wisata3.png" alt="Card image cap">
+			  			</a>
+			  			<div class="card-body">
+			   				<h5 class="card-title">Kabupaten Gianyar</h5>
+			   				<p class="card-text">Kabupaten Gianyar memiliki begitu banyak tempat wisata yang menarik.</p>
+			    			<a href="destination.php" class="btn btn-info">Go Explore</a>
+			 			</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm">
+				<div class="card">
+					<div class="popout">
+			  			<a data-fancybox="gallery" href="img/thumbs/wisata9.png">
+			  				<img class="card-img-top img img-responsive" src="img/thumbs/wisata9.png" alt="Card image cap">
+			  			</a>
+			  			<div class="card-body">
+			   				<h5 class="card-title">Kabupaten Bangli</h5>
+			   				<p class="card-text">Kabupaten Bangli memiliki begitu banyak tempat wisata yang menarik.</p>
+			    			<a href="destination.php" class="btn btn-info">Go Explore</a>
+			 			</div>
+			 		</div>
+				</div>
+			</div>
+			<div class="col-sm">
+				<div class="card">
+					<div class="popout">
+			  			<a data-fancybox="gallery" href="img/thumbs/wisata1.png">
+			  				<img class="card-img-top img img-responsive" src="img/thumbs/wisata1.png" alt="Card image cap">
+			  			</a>
+			  			<div class="card-body">
+			   				<h5 class="card-title">Kabupaten Karangasem</h5>
+			   				<p class="card-text">Kabupaten Karangasem memiliki begitu banyak tempat wisata yang menarik.</p>
+			    			<a href="destination.php" class="btn btn-info">Go Explore</a>
+			 			</div>
+			 		</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</section>
+	<!-- end portfolio -->
+
+	<!-- contact -->
+	<section id="contact" class="contact mb-5">
+		<div class="container">
+			<div class="row pt-4 mb-4">
+				<div class="col text-center">
+					<h1 class="display-4">Contact Us</h1>
+					<hr>
+				</div>
+			</div>
+			<div class="row justify-content-center">
+				<div class="col-sm-4">
+					<div class="card text-white bg-info mb-3 text-center">
+  						<div class="card-body">
+   							<h5 class="card-title">Contact Us</h5>
+    						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+						</div>
+					</div>
+					<ul class="list-group">
+					  <li class="list-group-item"><h1>Location</h1></li>
+					  <li class="list-group-item">My Office</li>
+					  <li class="list-group-item">Jl. Sangalangit</li>
+					  <li class="list-group-item">Penatih, Kec. Denpasar Timur, Kota Denpasar, Bali</li>
+					</ul>
+				</div>
+
+				<div class="col-sm-6">
+					<form>
+  						<div class="form-group">
+   							<label for="nama">Nama</label>
+   							<input type="text" class="form-control" id="nama" placeholder="Masukan Nama" autocomplete="off">
+  						</div>
+  						<div class="form-group">
+   							<label for="email">Email</label>
+   							<input type="text" class="form-control" id="email" placeholder="Masukan Email" autocomplete="off">
+  						</div>
+  						<div class="form-group">
+   							<label for="telp">No Telephone</label>
+   							<input type="text" class="form-control" id="telp" placeholder="Masukan Nomor Telpehone" autocomplete="off">
+  						</div>
+  						<select class="form-control">
+  							<option selected>-- Pilih Kategori --</option>
+  							<option>Tentang Kami</option>
+  							<option>Tentang Pemesanan</option>
+  							<option>Tentang Pasca Pemesanan</option>
+  							<option>Umpan Balik</option>
+  						</select>
+  						<div class="form-group">
+   							<label for="pesan">Pesan</label>
+   							<textarea name="pesan" id="pesan" class="form-control" rows="10" autocomplete="off"></textarea>
+  						</div>
+  						<div class="form-group">
+  							<button type="button" class="btn btn-info">Kirim Pesan</button>
+  						</div>
+					</form>
+				</div>
+			</div>	
+		</div>
+	</section>
+	<!-- end contact -->
+
+	<!-- footer -->
+	<footer class="bg-dark text-white">
+		<div class="container">
+			<div class="row pt-3">
+				<div class="col text-center ">
+					<p>Copyright 2020. | Kelompok 2</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- end footer -->
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="plugins/fancybox/jquery.fancybox.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
